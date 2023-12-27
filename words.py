72a8b90d741af871f742250df88a10e39183ef45
ghanis05 <154982500+ghanis05@users.noreply.github.com>
2023-12-27 18:12:43 +0530

    words.py

--- words.py
+++ words.py
@@ -1,12 +1,11 @@
 def can_form_word(letters, word):
-# Convert the letters and word to sets for easy comparison
-letter_set = set(letters.lower())
-word_set = set(word.lower())
-# Check if the word can be formed using the given letters
-return word_set.issubset(letter_set)
+  # Convert the letters and word to sets for easy comparison
+  letter_set = set(letters.lower())
+  word_set = set(word.lower())
+  # Check if the word can be formed using the given letters
+  return word_set.issubset(letter_set)
 # Given list of words
-word_list = ["Arun", "Varun", "Kent", "Eat", "Pot", "net", "Peak", "Peacock", "Zebra",
-"Nato", "Toe", "Poke", "Knife", "Peot", "Venus", "Ant"]
+word_list = ["Arun", "Varun", "Kent", "Eat", "Pot", "net", "Peak", "Peacock", "Zebra","Nato", "Toe", "Poke", "Knife", "Peot", "Venus", "Ant"]
 # Letters to check against
 letters_to_use = "AKEOTPN"
 # Find and print the words that can be formed
